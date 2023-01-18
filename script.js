0840c324130d684a525817b82bf4a1058d60b397
AminHossyn <mr.aminasady00@gmail.com>
2023-01-18 23:37:04 +0330

    I build home section

---
+++ script.js
@@ -0,0 +1,11 @@
+let navBar = document.querySelector('.header .navBar');
+console.log(navBar);
+
+document.querySelector('#menu-btn').addEventListener("click",()=>{
+    navBar.classList.toggle('active');
+})
+    
+    
+
+
+
